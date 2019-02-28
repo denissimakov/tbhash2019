a9539d339fb2e46f5df86d7ddc4774d7011de3ab
Denis Simakov <denis.simakov@gmail.com>
2019-02-28 21:18:35 +0200

    script 0

--- script_0.py
+++ script_0.py
@@ -3,11 +3,16 @@ input_path = 'input/e_shiny_selfies.txt'
 
 from scoring import *
 from simple_solutions import *
-from hash_io import Input_parser
+from hash_io import *
 images = Input_parser(input_path).images
 slides = dumb_solution(images)
 print(len(images), 'images')
 print(len(slides), 'slides')
 print('score=', score(slides))
 #list(all_pair_scores(slides))
+
+writer = Output_writer()
+writer = Output_writer(slides)
+writer.write_result_file('a_dumb.txt')
+
 
