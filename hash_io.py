d69a2df52a1dcb7815fcd94c4e943e7306ef2c02
ezakkay158088 <eyal_zakkay@amat.com>
2019-02-28 21:06:20 +0200

    output writer

--- hash_io.py
+++ hash_io.py
@@ -1,4 +1,5 @@
 from hash_classes import Image
+import os
 
 class Input_parser(object):
     """ Reads file from path, creates header string and a list of all lines in file (excluding header)"""
@@ -23,6 +24,7 @@ class Output_writer(object):
         self.slides = slides
 
     def write_result_file(self, path):
+        path = os.path.join('results_files', path)
         img_id_list = []
         for slide in self.slides:
             img_id_list.append([str(img.id) for img in slide.images])
