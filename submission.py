18d39206f7db46e4eccf71b276e0ed8f970bbcd6
Denis Simakov <denis.simakov@gmail.com>
2019-02-28 21:48:17 +0200

    refactor submission

--- submission.py
+++ submission.py
@@ -11,6 +11,17 @@ from hash_io import *
 import os
 import datetime
 
+def save_result(input_file, slides, score=None):
+    if score is None:
+        score = calc_score(slides)
+    writer = Output_writer(slides)
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+    name, ext = os.path.splitext(os.path.basename(input_file))
+    output_file = timestamp + '-' + name + '-score{}'.format(score) + ext
+    writer.write_result_file(output_file)
+    print(output_file)
+
+
 def run_on_all(solution_func):
     scores = [run_on_one(input_file=input_file, solution_func=solution_func) \
                 for input_file in sorted(input_paths)]
@@ -19,18 +30,13 @@ def run_on_all(solution_func):
 
 def run_on_one(input_file, solution_func):
     print(5*'-')
-    images = Input_parser(input_file).images
-    slides = solution_func(images)
     print(input_file)
+    images = Input_parser(input_file).images
     print(len(images), 'images')
+    slides = solution_func(images)
     print(len(slides), 'slides')
-    score_ = score(slides)
+    score_ = calc_score(slides)
     print('score=', score_)
-    writer = Output_writer(slides)
-    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S_%f")
-    name, ext = os.path.splitext(os.path.basename(input_file))
-    output_file = timestamp + '-' + name + '-score{}'.format(score_) + ext
-    writer.write_result_file(output_file)
-    print(output_file)
+    save_result(input_file, slides, score=score_)
     return score_
 
