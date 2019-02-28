639573c1cb60ecb95d13e27db79c309b918dc263
ezakkay158088 <eyal_zakkay@amat.com>
2019-02-28 20:33:29 +0200

    io

--- hash_io.py
+++ hash_io.py
@@ -5,6 +5,12 @@ class Input_parser(object):
         self.all_lines = self.f.readlines()
         self.header = self.all_lines.pop(0)
         self.n_lines = len(self.all_lines)  # excluding header
+        self.N = int(self.header)
+    def create_image_list(self):
+        self.images = []
+        for l in self.all_lines:
+            line = l.split()
+            self.images.append(Image(id=line[0], orientation=line[1], M=line[2], tags=set(line[3:])))
 
 class Output_writer(object):
 
@@ -13,7 +19,7 @@ class Output_writer(object):
 
 
 if __name__ == '__main__':
-    tmp = Input_parser('2019_practice/d_big.in')
+    tmp = Input_parser('input/a_example.txt')
     print('Header:')
     print(tmp.header)
     print('All lines:')
