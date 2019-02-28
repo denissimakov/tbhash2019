6db4dd92e69ee07e7d92d6ce4ae2f3555f9e31fd
Denis Simakov <denis.simakov@gmail.com>
2019-02-28 20:50:23 +0200

    rename

--- simple_solutions.py
+++ simple_solutions.py
@@ -1,6 +1,6 @@
 from hash_classes import *
 
-def dumb_solutions(images):
+def dumb_solution(images):
     horiz_images = [im for im in images if im.orientation=='H']
     vert_images = [im for im in images if im.orientation=='V']
     slides_horiz = [Slide((im,)) for im in horiz_images]
