6cd2f3ddb5d70c484cb64508fb29a9141735fd49
ezakkay158088 <eyal_zakkay@amat.com>
2019-02-28 19:10:29 +0200

    io abstract

---
+++ hash_io.py
@@ -0,0 +1,17 @@
+class Input_parser(object):
+
+    def __init__(self, path):
+        self.f = open(path, 'r')
+        self.all_lines = self.f.readlines()
+        self.header = self.all_lines.pop(0)
+
+
+class Output_writer(object):
+
+    def __init__(self, path):
+        pass
+
+
+# if __name__ == '__main__':
+#     tmp = Input_parser('/2019_practice/a_example.in')
+
