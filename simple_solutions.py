75ce8138bf0cee4420d35d5b63c9799dce8784a1
Denis Simakov <denis.simakov@gmail.com>
2019-02-28 22:41:04 +0200

    greedy solution to simple_solutions

--- simple_solutions.py
+++ simple_solutions.py
@@ -7,3 +7,46 @@ def dumb_solution(images):
     slides_vert = [Slide(vert_images[i:i+2]) for i in range(0,len(vert_images)-1,2)]
     slides = slides_horiz + slides_vert
     return slides
+
+def greedy_solution(images):
+    slides = [Slide((im,)) for im in images]
+    solution = [slides.pop(0)]
+    aaa = 0;
+    while len(slides) > 0:
+        max_score = 0
+        best = None
+        side = None
+        ind_best = 0
+        pos_right = len(solution[-1].tags)/2;
+        pos_left = len(solution[0].tags)/2;
+        for idx, s in enumerate(slides):
+            
+            
+            sr = score( [solution[-1],s])
+            if sr > max_score:
+                best = s
+                ind_best = idx
+                side = 'r'
+                max_score = sr
+                if max_score > 0:
+                    break
+            sl = score([s, solution[0]])
+            if  sl > max_score:
+                best = s
+                side = 'l'               
+                ind_best = idx
+                max_score = sl
+                if max_score > 0:
+                    break
+             
+        if best is None:
+            solution += [slides.pop(0)]
+        else:
+            if side == 'l':
+                solution = ([best] + solution)
+            else:
+                solution = ( solution + [best])
+            slides.pop(ind_best)
+        aaa += max_score    
+        print(len(slides),' ',aaa)
+    return solution
