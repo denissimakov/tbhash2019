b34185b23430b9d62b493ef92d728e1aee16ff3d
Denis Simakov <denis.simakov@gmail.com>
2019-02-28 20:44:37 +0200

    simple solution working

--- simple_solutions.py
+++ simple_solutions.py
@@ -1,8 +1,9 @@
 from hash_classes import *
 
-def dummy_solution(image_list):
-    horiz_images = [im for im in images if im.orientation=='h']
-    vert_images = [im for im in images if im.orientation=='v']
+def dummy_solution(images):
+    horiz_images = [im for im in images if im.orientation=='H']
+    vert_images = [im for im in images if im.orientation=='V']
     slides_horiz = [Slide((im,)) for im in horiz_images]
     slides_vert = [Slide(vert_images[i:i+2]) for i in range(0,len(vert_images)-1,2)]
     slides = slides_horiz + slides_vert
+    return slides
