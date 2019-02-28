7d028dc8205dda76bb454cc6209085f5f795a8ce
denissuh <dsuhanov13@gmail.com>
2019-02-28 20:23:51 +0200

    classes

---
+++ hash_classes.py
@@ -0,0 +1,7 @@
+
+class Image(object):
+    def __init__(self, id, orientation, tags, length):
+        self.id = id
+        self.orientation = orientation
+        self.tags = tags
+        self.length = length
